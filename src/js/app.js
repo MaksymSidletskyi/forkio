ee37100af20786e65e8fd90ddad47e6d4d21d921
Maxim Sidletskiy <sidmax2004@gmail.com>
2023-01-12 12:14:11 +0100

    add header 1-st time

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
 
+const button = document.querySelector('.menu__mobile');
+const menu = document.querySelector('.menu__mobile-hr')
+
+button.addEventListener('click', function () {
+   const navbar = document.querySelector('.nav__list');
+
+   console.log(navbar);
+    navbar.classList.toggle('active');
+
+    menu.classList.toggle('close');
+    menu.classList.toggle('menu__mobile-hr');
+
+})
+
